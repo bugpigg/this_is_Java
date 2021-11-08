d6037f6f4e3092be6815e684c31d77c193dbd1cd
bugpigg <bugpigg@gmail.com>
2021-11-08 22:13:53 +0900

    upload ch1

---
+++ chap1/src/exe2/Hello.java
@@ -0,0 +1,13 @@
+package exe2;
+
+/*
+ * 작성자: 장승운
+ * 작성일: 2021-11-08
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+		// 프로그램 실행 진입점, JVM은 main method를 먼저 실행시킨다.
+		System.out.println("Hello, world!");
+	}// end of main
+}// end of class
