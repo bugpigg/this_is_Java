70cb7dd478fb26aa01aa5df2c175c0daa09c7c87
bugpigg <bugpigg@gmail.com>
2021-11-08 23:44:47 +0900

    upload ch2

---
+++ chap2/src/ex/ByteExample.java
@@ -0,0 +1,15 @@
+package ex;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		byte var1 = -128;// minimum
+		byte var2 = 127; // maximum
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		
+
+	}
+
+}
