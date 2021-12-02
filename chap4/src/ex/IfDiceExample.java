9b868bf8c0bd160a7f554fa492d7b292708311c5
bugpigg <bugpigg@gmail.com>
2021-12-02 22:59:00 +0900

    ch4

---
+++ chap4/src/ex/IfDiceExample.java
@@ -0,0 +1,9 @@
+package ex;
+
+public class IfDiceExample {
+    public static void main(String[] args){
+        // 1~6 까지의 정수 중 임의의 정수
+        int num = (int)(Math.random()*6)+1;
+        System.out.println(num+"번이 나왔습니다.");
+    }
+}
---
+++ chap4/src/ex/SwitchStringExample.java
@@ -0,0 +1,13 @@
+package ex;
+
+public class SwitchStringExample {
+    public static void main(String[] args){
+        String pos = "사원";
+
+        switch (pos) {
+            case "부장" -> System.out.println("700만원");
+            case "과장" -> System.out.println("500만원");
+            default -> System.out.println("300만원");
+        }
+    }
+}
